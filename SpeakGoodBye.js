5decc4f829f023b50b26b4eea768b858cc0f1c23
movil90 <67565754+movil90@users.noreply.github.com>
2020-11-04 00:58:29 -0300

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window){
+    var byeSpeaker= {};
+    var speakWord = "Good bye";
+    byeSpeaker.speak= function (name) {
+        console.log (speakWord + " " + name)
+}
+    window.byeSpeaker= byeSpeaker;
+})(window);
